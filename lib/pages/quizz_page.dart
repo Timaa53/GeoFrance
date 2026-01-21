b0307d8c6e7bec80fac15b8a4be23445f84a5601
Timaa53 <cortinandy@gmail.com>
2026-01-21 12:01:29 +0100

    buttons OK, tests ok

--- lib/pages/quizz_page.dart
+++ lib/pages/quizz_page.dart
@@ -13,6 +13,24 @@ class QuizzPage extends StatefulWidget {
 class _QuizzPageState extends State<QuizzPage> {
   static const transparentBackground = Colors.transparent;
   Question currentQuestion = GameService().createQuestion();
+  Map<String, Color> buttonColors = {};
+
+  void checkAnswer(String selectedAnswer) {
+    setState(() {
+      if (selectedAnswer == currentQuestion.responseCorrect) {
+        buttonColors[selectedAnswer] = Colors.green;
+      } else {
+        buttonColors[selectedAnswer] = Colors.red;
+        buttonColors[currentQuestion.responseCorrect] = Colors.green;
+      }
+    });
+    Future.delayed(Duration(milliseconds: 1200), () {
+      setState(() {
+        currentQuestion = GameService().createQuestion();
+        buttonColors.clear();
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -27,47 +45,74 @@ class _QuizzPageState extends State<QuizzPage> {
         Scaffold(
           backgroundColor: transparentBackground,
           appBar: MyAppBar(),
-          body: Column(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              Row(
-                children: [
-                  Expanded(
-                    child: ElevatedButton(
-                      onPressed: () {},
-                      child: Text(currentQuestion.propositions[0]),
+          body: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Row(
+                  children: [
+                    Expanded(
+                      child: ElevatedButton(
+                        onPressed: () {
+                          checkAnswer(currentQuestion.propositions[0]);
+                        },
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor:
+                              buttonColors[currentQuestion.propositions[0]],
+                        ),
+                        child: Text(currentQuestion.propositions[0]),
+                      ),
                     ),
-                  ),
-                  Expanded(
-                    child: ElevatedButton(
-                      onPressed: () {},
-                      child: Text(currentQuestion.propositions[1]),
+                    SizedBox(width: 20),
+                    Expanded(
+                      child: ElevatedButton(
+                        onPressed: () {
+                          checkAnswer(currentQuestion.propositions[1]);
+                        },
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor:
+                              buttonColors[currentQuestion.propositions[1]],
+                        ),
+                        child: Text(currentQuestion.propositions[1]),
+                      ),
                     ),
-                  ),
-                ],
-              ),
-              Row(
-                children: [
-                  Expanded(
-                    child: ElevatedButton(
-                      onPressed: () {},
-                      child: Text(currentQuestion.propositions[2]),
+                  ],
+                ),
+                Row(
+                  children: [
+                    Expanded(
+                      child: ElevatedButton(
+                        onPressed: () {
+                          checkAnswer(currentQuestion.propositions[2]);
+                        },
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor:
+                              buttonColors[currentQuestion.propositions[2]],
+                        ),
+                        child: Text(currentQuestion.propositions[2]),
+                      ),
                     ),
-                  ),
-                  Expanded(
-                    child: ElevatedButton(
-                      onPressed: () {},
-                      child: Text(currentQuestion.propositions[3]),
+                    SizedBox(width: 20),
+                    Expanded(
+                      child: ElevatedButton(
+                        onPressed: () {
+                          checkAnswer(currentQuestion.propositions[3]);
+                        },
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor:
+                              buttonColors[currentQuestion.propositions[3]],
+                        ),
+                        child: Text(currentQuestion.propositions[3]),
+                      ),
                     ),
-                  ),
-                ],
-              ),
-            ],
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ],
     );
   }
-
-  chechAnswer(String) {}
 }
